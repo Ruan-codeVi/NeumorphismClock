4d3ec56eace13e5882c18bad5d4360d75a212a38
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-03-28 17:33:33 -0300

    up

---
+++ script/script.js
@@ -0,0 +1,19 @@
+         function toggleClass(){
+             const body = document.querySelector('body');
+             body.classList.toggle('Luz')
+         }
+         const deg = 6;
+          const hr = document.querySelector('#hr');
+          const min = document.querySelector('#min');
+          const seg = document.querySelector('#seg')
+          
+          setInterval(()=>{
+          let day = new Date();
+          let hh = day.getHours() * 30;
+          let mm = day.getMinutes() * deg;
+          let ss = day.getSeconds() * deg;
+
+          hr.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
+          min.style.transform = `rotateZ(${mm}deg)`;
+          seg.style.transform = `rotateZ(${ss}deg)`;
+          })
